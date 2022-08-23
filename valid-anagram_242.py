1305d44bac692a94171b45dbc2fb2e4f5f5f2a0a
Ali Akkas <aliakkas006@gmail.com>
2022-08-23 20:13:02 +0600

    valid-anagram-242

---
+++ valid-anagram_242.py
@@ -0,0 +1,24 @@
+str_1 = 'anagram'
+str_2 = 'nagaram'
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # solution->1: using sorted function
+        # return sorted(s) == sorted(t)
+
+        # solution->2: using hashmap
+        if(len(s) != len(t)):
+            return False
+        
+        count_S, count_T = {}, {}
+
+        for i in range(len(s)):
+            count_S[s[i]] = 1+ count_S.get(s[i], 0)
+            count_T[t[i]] = 1+ count_T.get(t[i], 0)
+
+        return count_S == count_T
+
+instance = Solution()
+result = instance.isAnagram(str_1, str_2)
+
+print(result)
