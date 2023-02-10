51e4b4f48bbcb4a4f0da933f61c471a578f794d7
Ali Akkas <aliakkas006@gmail.com>
2023-02-10 20:10:30 +0600

    container-with-most-water solved

---
+++ container-with-most-water_11.py
@@ -0,0 +1,19 @@
+height = [1,8,6,2,5,4,8,3,7]
+
+def maxArea(height):
+    max_area = 0
+    left, right = 0, len(height) - 1
+
+    while left < right:
+        area = (right - left) * min(height[left], height[right])
+        max_area = max(max_area, area)
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    
+    return max_area
+
+
+print('Maximum Area =', maxArea(height))
