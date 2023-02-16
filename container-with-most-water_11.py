2a8b097d7f0315d957f66b85b9a995c9091c1cb9
Ali Akkas <aliakkas006@gmail.com>
2023-02-16 22:35:56 +0600

    number-of-1-bits problem solved

--- container-with-most-water_11.py
+++ container-with-most-water_11.py
@@ -5,8 +5,8 @@ def maxArea(height):
     left, right = 0, len(height) - 1
 
     while left < right:
-        area = (right - left) * min(height[left], height[right])
-        max_area = max(max_area, area)
+        current_area = (right - left) * min(height[left], height[right])
+        max_area = max(max_area, current_area)
 
         if height[left] < height[right]:
             left += 1
