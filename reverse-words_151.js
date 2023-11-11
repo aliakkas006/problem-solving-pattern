ef917ebde12e904eaa14e758438a564cc3a3c047
Ali Akkas <aliakkas006@gmail.com>
2023-11-11 23:09:34 +0600

    feat: solved trapping rain water problem

--- reverse-words_151.js
+++ reverse-words_151.js
@@ -12,16 +12,20 @@ const reverseWords = (s) => {
   const revStr = revWords.join(' ');
 
   return revStr;
-  
-  //   const splitStr = s.trim().replace(/\s+/g, ' ').split(' ')
-  //   let j = 0;
-  //   let arr = [];
-
-  //   for (let i = splitStr.length - 1; i >= 0; i--) {
-  //     arr[j++] = splitStr[i];
-  //   }
-
-  //   return arr.join(' ')
 };
 
 console.log(reverseWords('a good   example'));
+
+/* 
+const reverseWords = (s) => {
+  const splitStr = s.trim().replace(/\s+/g, ' ').split(' ');
+  let j = 0;
+  let arr = [];
+
+  for (let i = splitStr.length - 1; i >= 0; i--) {
+    arr[j++] = splitStr[i];
+  }
+
+  return arr.join(' ');
+};
+ */
